2a15f28f0b88f7bcdeb11a407237f5fe6adaa6cc
Denrage <denrage@gmail.com>
2019-10-09 10:17:05 +0200

    Added dummy for returning timetable in grpc service

--- src/Modules/Module.Hsnr/Server/Module.cs
+++ src/Modules/Module.Hsnr/Server/Module.cs
@@ -4,6 +4,11 @@ using System.Threading.Tasks;
 using Cida.Api;
 using Grpc.Core;
 using Hsnr;
+using Module.Hsnr.Extensions;
+using Module.Hsnr.Timetable;
+using Module.Hsnr.Timetable.Data;
+using Module.Hsnr.Timetable.Parser;
+using SemesterType = Module.Hsnr.Timetable.Data.SemesterType;
 
 namespace Module.Hsnr
 {
@@ -31,6 +36,26 @@ namespace Module.Hsnr
 
     public class HsnrTimetableServiceImplementation : HsnrTimetableService.HsnrTimetableServiceBase
     {
-        
+        private readonly Timetable.TimetableService timetableService;
+
+        public HsnrTimetableServiceImplementation()
+        {
+            this.timetableService = new TimetableService(new WeekDayParser(new TimetableTimeParser(), new SubjectParser()));
+        }
+
+        public override async Task<TimetableResponse> Timetable(TimetableRequest request, ServerCallContext context)
+        {
+            var result = (await this.timetableService.GetTimetableAsync(new FormData()
+            {
+                Calendar = CalendarType.BranchOfStudy,
+                Semester = SemesterType.WinterSemester,
+                BranchOfStudy = "KBI4",
+            })).ToGrpc();
+            
+            return new TimetableResponse()
+            {
+                Result = result, 
+            };
+        }
     }
 }
