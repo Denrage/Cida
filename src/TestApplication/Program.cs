5550196130d6f3c8ee7c882576cd24171f08a5e6
Denrage <denrage@gmail.com>
2019-10-15 14:09:44 +0200

    Added Hsnr timetable module communication to TestApplication

--- src/TestApplication/Program.cs
+++ src/TestApplication/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Text;
 using Grpc.Core;
+using Hsnr;
 
 namespace TestApplication
 {
@@ -9,10 +11,30 @@ namespace TestApplication
         {
             Console.WriteLine("Hello World!");
 
-            var channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
-            var client = new Cida.CidaApiService.CidaApiServiceClient(channel);
-            Console.WriteLine(client.Version(new Cida.VersionRequest()).Version);
+            var channel = new Channel("127.0.0.1:31564", ChannelCredentials.Insecure);
+//            var client = new Cida.CidaApiService.CidaApiServiceClient(channel);
+//            Console.WriteLine(client.Version(new Cida.VersionRequest()).Version);
+            var client2 = new HsnrTimetableService.HsnrTimetableServiceClient(channel);
+            var response = client2.Timetable(new TimetableRequest()
+            {
+                Calendar = CalendarType.BranchOfStudy,
+                BranchOfStudy = "KBI5",
+                Semester = SemesterType.WinterSemester,
+            });
 
+            foreach (var weekDay in response.Result.WeekDays)
+            {
+                Console.WriteLine(weekDay.Day);
+                    var stringBuilder = new StringBuilder();
+                foreach (var subject in weekDay.Subjects)
+                {
+                    stringBuilder.Append(subject.Start).Append(" - ").Append(subject.End).Append("\t")
+                        .Append(subject.Name).Append("\t").Append(subject.Room).Append("\t")
+                        .AppendLine(subject.Lecturer);
+                }
+                Console.WriteLine(stringBuilder.ToString() + "\n\n");
+            }
+            
 
             Console.ReadKey();
             channel.ShutdownAsync().Wait();
