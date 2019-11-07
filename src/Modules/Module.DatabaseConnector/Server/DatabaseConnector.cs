dbb832ee5b9ef07a0698c407c713f39f82d8f59e
Unknown <flangi@gmx.de>
2019-11-07 15:46:01 +0100

    Made DatabaseConnector async

--- src/Modules/Module.DatabaseConnector/Server/DatabaseConnector.cs
+++ src/Modules/Module.DatabaseConnector/Server/DatabaseConnector.cs
@@ -1,4 +1,4 @@
-using Microsoft.Data.SqlClient;
+﻿using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Module.DatabaseConnector.EFC;
 using Module.DatabaseConnector.Models.DatabaseModels;
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Data.Common;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Module.DatabaseConnector
 {
@@ -53,12 +54,62 @@ namespace Module.DatabaseConnector
                 moduleInformation.Password = password;
                 moduleInformation.DatabaseName = $"ModuleDb_{moduleId}";
                 this.context.SaveChanges();
+
+            if (moduleInformation.Password != password)
+            {
+                throw new Exception("Wrong login");
+            }
+            }
+
+
+            var connectionStringBuilder = new SqlConnectionStringBuilder
+            {
+                DataSource = dbConnection.DataSource,
+                InitialCatalog = moduleInformation.DatabaseName,
+                UserID = moduleInformation.Username,
+                Password = moduleInformation.Password
+            };
+
+            return connectionStringBuilder.ToString();
+        }
+
+        public async Task<string> GetDatabaseConnectionStringAsync(string moduleId, string password)
+        {
+            var dbConnection = this.databaseConnectionProvider.GetDatabaseConnection();
+
+            var modules = this.context.Modules;
+            var moduleInformation = await modules.FindAsync(moduleId);
+
+            if (moduleInformation == null)
+            {
+                await this.CreateDatabaseAsync(moduleId, password);
+                moduleInformation = new ModuleInformation()
+                {
+                    ModuleId = moduleId,
+                    ModuleName = "temp",
+                    Username = $"ModuleUser_{moduleId}",
+                    Password = password,
+                    DatabaseName = $"ModuleDb_{moduleId}"
+                };
+
+                await modules.AddAsync(moduleInformation);
+                await this.context.SaveChangesAsync();
             }
 
             else if (moduleInformation.DatabaseName == null)
             {
-                throw new Exception("Wrong login");
+                await this.CreateDatabaseAsync(moduleId, password);
+                moduleInformation.Username = $"ModuleUser_{moduleId}";
+                moduleInformation.Password = password;
+                moduleInformation.DatabaseName = $"ModuleDb_{moduleId}";
+                await this.context.SaveChangesAsync();
+
+                if (moduleInformation.Password != password)
+                {
+                    throw new Exception("Wrong login");
+                }
             }
+
 
             var connectionStringBuilder = new SqlConnectionStringBuilder
             {
@@ -93,6 +144,31 @@ namespace Module.DatabaseConnector
                 this.context.Database.ExecuteSqlRaw(createUserSql);
                 transaction.Commit();
                 dbConnection.Close();
+            }
+        }
+
+        private async Task CreateDatabaseAsync(string moduleId, string password)
+        {
+            var dbName = $"ModuleDb_{moduleId}";
+            var username = $"ModuleUser_{moduleId}";
+
+            using (var dbConnection = this.databaseConnectionProvider.GetDatabaseConnection())
+            {
+                await dbConnection.OpenAsync();
+                var transaction = await dbConnection.BeginTransactionAsync();
+
+                var createDbSql = $@"   CREATE DATABASE {dbName};
+                                ";
+                var createUserSql = $@" USE [master];
+                                    CREATE LOGIN [{username}] WITH PASSWORD=N'{password}', CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF;
+                                    USE [{dbName}];
+                                    CREATE USER [{username}] FOR LOGIN [{username}];
+                                    ALTER ROLE [db_owner] ADD MEMBER [{username}];
+                                ";
+                await this.context.Database.ExecuteSqlRawAsync(createDbSql);
+                await this.context.Database.ExecuteSqlRawAsync(createUserSql);
+                await transaction.CommitAsync();
+                await dbConnection.CloseAsync();
             }
         }
     }
