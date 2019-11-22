f4be8f9e6e4fb41b50686f36e3ff333b8db39e53
Unknown <flangi@gmx.de>
2019-11-22 08:47:53 +0100

    Reorganization

---
+++ src/Modules/Module.DatabaseConnector/Server/Models/DatabaseModels/FtpInformation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module.DatabaseConnector.Models.DatabaseModels
+{
+    public class FtpInformation
+    {
+        public string ModuleId { get; set; }
+
+        public string FtpPath { get; set; }
+    }
+}
--- src/Projects/Cida.Server/Infrastructure/Database/EFC/CidaContext.cs
+++ src/Projects/Cida.Server/Infrastructure/Database/EFC/CidaContext.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Module.DatabaseConnector.Models.DatabaseModels;
-using Module.DatabaseConnector.Settings;
+﻿using Cida.Server.Infrastructure.Database;
+using Cida.Server.Infrastructure.Database.Models.DatabaseModels;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Module.DatabaseConnector.EFC
+namespace Cida.Server.Infrastructure.Database.EFC
 {
     public class CidaContext : DbContext
     {
@@ -19,6 +19,8 @@ namespace Module.DatabaseConnector.EFC
         public DbSet<ModuleInformation> Modules { get; set; }
 
         public DbSet<FtpInformation> FtpPaths { get; set; }
+
+        public DbSet<DatabaseInformation> Databases { get; set; }
 
 
 
@@ -36,18 +38,29 @@ namespace Module.DatabaseConnector.EFC
             modelBuilder.Entity<ModuleInformation>(moduleInformation =>
             {
                 moduleInformation.HasKey(x => x.ModuleId);
+                moduleInformation.HasOne(x => x.FtpInfomation).WithOne(x => x.Module);
+
                 moduleInformation.Property(x => x.ModuleId).IsRequired().HasComment("Unique ID of a Cida module.");
                 moduleInformation.Property(x => x.ModuleName).IsRequired().HasComment("Name of the module with the given ID.");
-                moduleInformation.Property(x => x.DatabaseName).HasComment("Database name for login.");
-                moduleInformation.Property(x => x.Username).HasComment("Username used for login to the given database.");
-                moduleInformation.Property(x => x.Password).HasComment("Password used for login to the given database.");
             });
 
             modelBuilder.Entity<FtpInformation>(ftpInformation =>
             {
+                ftpInformation.HasOne(x => x.Module).WithOne(x => x.FtpInfomation).HasForeignKey<FtpInformation>(x => x.ModuleId).IsRequired();
                 ftpInformation.HasKey(x => x.ModuleId);
+
                 ftpInformation.Property(x => x.ModuleId).IsRequired().HasComment("Unique ID of a Cida modle.");
                 ftpInformation.Property(x => x.FtpPath).IsRequired().HasComment("Path of the module on FTP-Server");
+            });
+
+            modelBuilder.Entity<DatabaseInformation>(databaseInformation =>
+            {
+                databaseInformation.HasOne(x => x.Module).WithOne(x => x.DatabaseInformation).HasForeignKey<DatabaseInformation>(x => x.ModuleId).IsRequired();
+                databaseInformation.HasKey(x => x.ModuleId);
+
+                databaseInformation.Property(x => x.DatabaseName).HasComment("Database name for login.");
+                databaseInformation.Property(x => x.Username).HasComment("Username used for login to the given database.");
+                databaseInformation.Property(x => x.Password).HasComment("Password used for login to the given database.");
             });
         }
     }
