ff465575b4ff074f0166bf2516df5ae8a41ad107
Denrage <denrage@gmail.com>
2019-12-29 23:39:44 +0100

    Added avalonia client

---
+++ src/Projects/Cida.Client.Avalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace Cida.Client.Avalonia.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
