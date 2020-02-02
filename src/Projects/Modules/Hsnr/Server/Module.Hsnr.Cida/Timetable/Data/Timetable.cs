2a58abeca2bf10fb2e4f55451599cc04aba339f2
Denrage <denrage@gmail.com>
2020-02-02 23:24:28 +0100

    Removed obsolet field in Timetable

--- src/Projects/Modules/Hsnr/Server/Module.Hsnr.Cida/Timetable/Data/Timetable.cs
+++ src/Projects/Modules/Hsnr/Server/Module.Hsnr.Cida/Timetable/Data/Timetable.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace Module.Hsnr.Timetable.Data
 {
@@ -10,15 +11,13 @@ namespace Module.Hsnr.Timetable.Data
         
         public SemesterType Semester { get; }
         
-        private readonly IEnumerable<WeekDay> weekDays;
-        
         public IReadOnlyList<WeekDay> WeekDays { get; }
 
         public Timetable(CalendarType type, SemesterType semester, IEnumerable<WeekDay> weekDays)
         {
             this.Type = type;
             this.Semester = semester;
-            this.WeekDays = new ReadOnlyCollection<WeekDay>(this.weekDays.ToList());
+            this.WeekDays = new ReadOnlyCollection<WeekDay>(weekDays.ToList());
         }
     }
 }
