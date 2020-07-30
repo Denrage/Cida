ba3b9755cd83cf69c8858212581d75ea4a8e3f6e
Denrage <denrage@gmail.com>
2020-07-30 17:27:37 +0200

    Added horriblesubs service

---
+++ src/Projects/Modules/HorribleSubs/Server/Module.HorribleSubs.Cida/Module.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cida.Api;
+using Grpc.Core;
+using Horriblesubs;
+
+namespace Module.Crunchyroll.Cida
+{
+    public class Module : IModule
+    {
+        // Hack: Remove this asap
+        private const string DatabasePassword = "HorribleSubs";
+        private const string Id = "109F8F54-DE94-479A-840A-7B4EF0F284D2";
+        private string connectionString;
+        public IEnumerable<ServerServiceDefinition> GrpcServices { get; private set; } = Array.Empty<ServerServiceDefinition>();
+
+        public async Task Load(IDatabaseConnector databaseConnector, IFtpClient ftpClient)
+        {
+            this.connectionString =
+                await databaseConnector.GetDatabaseConnectionStringAsync(Guid.Parse(Id), DatabasePassword);
+            this.GrpcServices = new[] {HorribleSubsService.BindService(new HorribleSubsImplementation()), };
+            Console.WriteLine("Loaded CR");
+        }
+
+        public class HorribleSubsImplementation : HorribleSubsService.HorribleSubsServiceBase
+        {
+            public HorribleSubsImplementation()
+            {
+            }
+        }
+    }
+}
